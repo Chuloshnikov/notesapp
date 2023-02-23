95f44c43979bd6c985d4a705d26c785ee7fc2d5a
MaxChuloshnikov <Maks447@ukr.net>
2023-02-23 16:59:36 +0200

    SearchBar Done
    Adaptive Fix Need

--- src/pages/Notes.jsx
+++ src/pages/Notes.jsx
@@ -1,6 +1,7 @@
-import React, { useState } from 'react';
-import { Section, NotesHeader, Btn, NotesContainer, AddLink } from './Notes.styled';
+import React, { useState, useEffect } from 'react';
+import { Section, NotesHeader, Btn, NotesContainer, AddLink, EmptyNotes } from './Notes.styled';
 import { CiSearch } from 'react-icons/ci';
+import { MdClose } from 'react-icons/md';
 import { BsPlusLg } from 'react-icons/bs';
 import NodesItem from '../components/NodesItem';
 
@@ -12,16 +13,30 @@ const Notes = ({ notes }) => {
   const [text, setText] = useState('');
   const [filteredNotes, setFilteredNotes] = useState(notes);
 
+  const handleSearch = () => {
+    setFilteredNotes(notes.filter((note) => {
+      if (note.title.toLowerCase().match(text.toLocaleLowerCase())){
+        return note;
+      }
+    }))
+  }
+
+  useEffect(handleSearch, [text]);
+
   return (
     <Section>
       <NotesHeader>
         {!showSearch && <h2>My Notes</h2>}
-        {showSearch && <input type="text" autoFocus placeholder='Search...' onChange={(event) => setText(event.target.value)}/>}  
-        <Btn onClick={() => setShowSearch(!showSearch)}><CiSearch/></Btn>
+        {showSearch && <input type="text" autoFocus placeholder='Search...' onChange={(event) => {
+          setText(event.target.value);
+          handleSearch();
+           }}/>}  
+        <Btn onClick={() => setShowSearch(!showSearch)}>{showSearch ? <MdClose/> : <CiSearch/>}</Btn>
         <AddLink to="/create-note"><BsPlusLg/></AddLink>
       </NotesHeader>
       <NotesContainer>
-        {notes.map((note) => (
+        {filteredNotes.length === 0 && <EmptyNotes>No Notes found</EmptyNotes>}
+        {filteredNotes.map((note) => (
           <NodesItem key={note.id} note={note}>
 
           </NodesItem>
